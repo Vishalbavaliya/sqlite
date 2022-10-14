a89da37ba286ae1f2674d32dcd8cc55dcc241203
Hiren Chauhan <hirenchauhan1857@gamil.com>
2022-10-14 10:37:01 +0530

    all

--- OurosJewels/AppDelegate.swift
+++ OurosJewels/AppDelegate.swift
@@ -6,31 +6,82 @@
 //
 
 import UIKit
+import SQLite3
+
+var dbQueue: OpaquePointer!
+var dbURL = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        dbQueue = cerateAndOpenDatabase()
+        
+        if (createTables() == false) {
+            print("Error in Createing Table")
+        } else {
+            print("YAY! Table Created")
+        }
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    func cerateAndOpenDatabase() -> OpaquePointer? {
+        
+        var db: OpaquePointer?
+        let url = NSURL(fileURLWithPath: dbURL)
+        
+        if let pathComponent = url.appendingPathComponent("TEST.sqlite") {
+            let filePath = pathComponent.path
+            if sqlite3_open(filePath, &db) == SQLITE_OK {
+                print("Successfully opened the database :) at \(filePath)")
+                return db
+            } else {
+                print("Could not open Database")
+            }
+        } else {
+            print("File path is not available")
+        }
+        return db
+    }
+    
+    func createTables() -> Bool {
+        var bRetVal: Bool = false
+        let createTable = sqlite3_exec(dbQueue, "CREATE TABLE IF NOT EXISTS TEMP (TEMPCOLUMN1 TEXT NULL, TEMPCOLUMN2 TEXT NULL)", nil, nil, nil)
+        if(createTable != SQLITE_OK) {
+            print("Not able to table create")
+            bRetVal = false
+        } else {
+            bRetVal = true
+        }
+        return bRetVal
+    }
+    
 }
 
+//        let selectStatementString = "SELECT TEMPCOLUMN1, TEMPCOLUMN2 FROM TEMP"
+//        var selectStatementQuery: OpaquePointer?
+//        var sShowData: String!
+//        sShowData = ""
+//
+//        if sqlite3_prepare_v2(dbQueue, selectStatementString, -1, &selectStatementQuery, nil) == SQLITE_DONE {
+//            while sqlite3_step(selectStatementQuery) == SQLITE_ROW {
+//                sShowData += String(cString: sqlite3_column_text(selectStatementQuery, 0)) + "\t\t" + String(cString:sqlite3_column_text(selectStatementQuery, 1)) + "\n"
+//            }
+//            sqlite3_finalize(selectStatementQuery)
+//        }
--- OurosJewels/ViewController.swift
+++ OurosJewels/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController {
-
+    
+    let SQLITE_TRANSIENT = unsafeBitCast(-1, to: sqlite3_destructor_type.self)
+    
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var sarNameTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    @IBAction func saveButton(_ sender: Any) {
+        let insertStatementString = "INSERT INTO TEMP (TEMPCOLUMN1, TEMPCOLUMN2) VALUES (?, ?);"
+        var insertStatementQuery : OpaquePointer?
+        if(sqlite3_prepare_v2(dbQueue, insertStatementString, -1, &insertStatementQuery, nil)) == SQLITE_OK {
+            sqlite3_bind_text(insertStatementQuery, 1, nameTF.text ?? "", -1, SQLITE_TRANSIENT)
+            sqlite3_bind_text(insertStatementQuery, 2, sarNameTF.text ?? "", -1, SQLITE_TRANSIENT)
+            
+            if(sqlite3_step(insertStatementQuery)) == SQLITE_DONE {
+                
+                nameTF.text = ""
+                sarNameTF.text = ""
+                
+                nameTF.becomeFirstResponder()
+                print("Successfully inserted the Record")
+            } else {
+                print("Error")
+            }
+            sqlite3_finalize(insertStatementQuery)
+        }
+    }
+    
+    @IBAction func updateButton(_ sender: Any) {
+        let updateStatementString = "UPDATE TEMP SET TEMPCOLUMN2 = '\(sarNameTF.text!)' WHERE TEMPCOLUMN1 = '\(nameTF.text!)';"
+        var updateStatement: OpaquePointer?
+        if sqlite3_prepare_v2(dbQueue, updateStatementString, -1, &updateStatement, nil) == SQLITE_OK {
 
-
+            if sqlite3_step(updateStatement) == SQLITE_DONE {
+                print("\nSuccessfully updated row.")
+            } else {
+                print("\nCould not update row.")
+            }
+        } else {
+            print("\nUPDATE statement is not prepared")
+        }
+        sqlite3_finalize(updateStatement)
+    }
 }
+
 
